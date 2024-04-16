fe648fd68bf7748dc1034b07d3cc4f2a9d5c7e1c
Sam Woodman <sam.woodman@noaa.gov>
2024-04-15 20:17:22 -0700

    initial package build

---
+++ R/shade-package.R
@@ -0,0 +1,13 @@
+#' shade: Shiny Shadow Fixer
+#'
+#' R package/Shiny app for fixing VIAME CSV output files. See [this bug](https://github.com/Kitware/dive/issues/1415) for more details
+#'
+#' @name shade-package
+#' @aliases shade
+#' @title Shiny Shadow Fixer
+#' @author Sam Woodman \email{sam.woodman@@noaa.gov}
+#'
+#' @import shiny
+#'
+#' @keywords package
+"_PACKAGE"
